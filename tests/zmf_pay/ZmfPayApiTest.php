196bfa5b08923bdbab27a47bef6e4b56549a3581
peter <perter@126.com>
2019-10-17 18:00:37 +0800

    登录注册增加vip等级

--- tests/zmf_pay/ZmfPayApiTest.php
+++ tests/zmf_pay/ZmfPayApiTest.php
@@ -8,17 +8,8 @@
 
 namespace byTest\component\zmf_pay;
 
-use by\component\encrypt\rsa\Rsa;
+use by\component\message_sender\impl\SubmailSmsSender;
 use by\component\usdt_pay\UsdtPay;
-use by\component\zmf_pay\common\ZmfDevConfig;
-use by\component\zmf_pay\common\ZmfProductCode;
-use by\component\zmf_pay\common\ZmfTools;
-use by\component\zmf_pay\req\ZmfBankBranchQueryReq;
-use by\component\zmf_pay\req\ZmfCustomerBankQueryReq;
-use by\component\zmf_pay\req\ZmfFourFactorsAuthReq;
-use by\component\zmf_pay\req\ZmfProductQueryReq;
-use by\component\zmf_pay\ZmfPayApi;
-use by\infrastructure\base\CallResult;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
 class ZmfPayApiTest extends TestCase
@@ -27,6 +18,18 @@ class ZmfPayApiTest extends TestCase
     public function testIndex() {
         $url = (new UsdtPay())->getPayUrl('1', '500');
         var_dump($url);
+    }
+
+    public function testSms() {
+        $data = [
+            'appid' => '',
+            'appkey' => '',
+            'code' => '123456',
+            'content' => '【SUBMAIL】您的短信验证码：4438，请在10分钟内输入。',
+            'mobile' => '12345678900',
+        ];
+        $ret = (new SubmailSmsSender($data))->send();
+        var_dump($ret);
     }
 
 }
