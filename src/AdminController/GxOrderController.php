48a4991c6b78d37ce4674b061ae19d2239bfbe43
peter <perter@126.com>
2019-11-06 13:51:27 +0800

    增加支付配置

--- src/AdminController/GxOrderController.php
+++ src/AdminController/GxOrderController.php
@@ -7,6 +7,7 @@ namespace App\AdminController;
 use App\Common\GxGlobalConfig;
 use App\Entity\GxOrder;
 use App\Entity\ProfitGraph;
+use App\Entity\UserAccount;
 use App\Helper\CodeGenerator;
 use App\ServiceInterface\GxOrderServiceInterface;
 use App\ServiceInterface\ProfitGraphServiceInterface;
@@ -42,15 +43,22 @@ class GxOrderController extends BaseNeedLoginController
     /**
      * 查询订单
      * @param PagingParams $pagingParams
+     * @param string $mobile
      * @param string $orderNo
      * @return CallResult|string
      * @throws NotLoginException
      */
-    public function query(PagingParams $pagingParams, $orderNo = '') {
+    public function query(PagingParams $pagingParams, $mobile = '', $orderNo = '') {
         $this->checkLogin();
         $map = [];
         if (!empty($orderNo)) {
             $map['order_no'] = ['like', '%'.$orderNo.'%'];
+        }
+        if (!empty($mobile)) {
+            $ua = $this->userAccountService->info(['mobile' => $mobile]);
+            if ($ua instanceof UserAccount) {
+                $map['uid'] = $ua->getId();
+            }
         }
 
         return $this->gxOrderService->queryAndCount($map, $pagingParams, ["id" => "desc"]);
