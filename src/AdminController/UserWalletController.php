0581d5161c40ff8508960358188a769d6f39ad5d
peter <perter@126.com>
2019-11-07 11:45:56 +0800

    增加支付配置

---
+++ src/AdminController/UserWalletController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\AdminController;
+
+use Dbh\SfCoreBundle\Common\UserLogServiceInterface;
+use App\ServiceInterface\UserWalletLogServiceInterface;
+use App\ServiceInterface\UserWalletServiceInterface;
+use by\component\paging\vo\PagingParams;
+use Dbh\SfCoreBundle\Common\LoginSessionInterface;
+use Dbh\SfCoreBundle\Common\UserAccountServiceInterface;
+use Dbh\SfCoreBundle\Controller\BaseNeedLoginController;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+class UserWalletController extends BaseNeedLoginController
+{
+    protected $walletService;
+    protected $logService;
+    protected $userLogService;
+
+    public function __construct(
+        UserLogServiceInterface $userLogService,
+        UserWalletLogServiceInterface $logService,
+        UserWalletServiceInterface $walletService,
+        UserAccountServiceInterface $userAccountService, LoginSessionInterface $loginSession, KernelInterface $kernel)
+    {
+        parent::__construct($userAccountService, $loginSession, $kernel);
+        $this->walletService = $walletService;
+        $this->logService = $logService;
+        $this->userLogService = $userLogService;
+    }
+
+    /**
+     * @param PagingParams $pagingParams
+     * @return mixed
+     * @throws \by\component\exception\NotLoginException
+     */
+    public function queryLogHistory(PagingParams $pagingParams)
+    {
+        $this->checkLogin();
+        return $this->logService->queryAndCount(['uid' => $this->getUid()], $pagingParams, ["id" => "desc"]);
+    }
+
+}
